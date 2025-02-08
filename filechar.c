7f8c02721a9042e182e3cb98826ba49f5a67facb
BenjaminSisk <siskb@purdue.edu>
2025-02-07 23:46:11 -0500

    Final

--- filechar.c
+++ filechar.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
 bool countChar(char * filename, int * counts, int size)
 {
+  FILE * fid;
+  fid = fopen(filename, "r");
+
   // open a file whose name is filename for reading
+  if (fid == NULL)
+  {
+    printf("Come on you guys, there it is right there in front of you the whole time. You're dereferencing a null pointer! Open your eyes!");
+    return false;
+  }
+
   // if fopen fails, return false. Do NOT fclose
   // if fopen succeeds, read every character from the file
-  //
+
+  int onechar;
+  while((onechar = fgetc(fid)) != EOF)
+  {
+    if (onechar >= 0 && onechar <= size - 1)
+    {
+      counts[onechar] += 1;
+    }
+  }
   // if a character (call it onechar) is between
   // 0 and size - 1 (inclusive), increase
   // counts[onechar] by one
@@ -21,12 +37,26 @@ bool countChar(char * filename, int * counts, int size)
   // Please read the document of fgetc carefully, in particular
   // when reaching the end of the file
   //
+  fclose(fid);
+  //remove(filename);
+  
   return true;
 }
 
 
 void printCounts(int * counts, int size)
 {
+  int ind;
+  int onechar;
+
+  for(ind = 0; ind <= size - 1; ind++)
+  {
+    if (counts[ind] != 0)
+    {
+      onechar = ((ind >= 65 && ind <= 90 )|| (ind >= 97 && ind <= 122)) ? ind : 32;
+      printf("%d, %c, %d\n", ind, onechar, counts[ind]);
+    }
+  }
   // print the values in counts in the following format
   // each line has three items:
   // ind, onechar, counts[ind]
--- fileint.c
+++ fileint.c
@@ -1,28 +1,58 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
 bool addFile(char * filename, int * sum)
 {
+  FILE * fid;
+  * sum = 0;
   // You cannot assume * sum is zero. Thus, * sum needs to be set 0
+  fid = fopen(filename, "r");
   // open a file whose name is filename for reading
+  if (fid == NULL)
+  {
+    printf("Come on you guys, there it is right there in front of you the whole time. You're dereferencing a null pointer! Open your eyes!");
+    return false;
+  }
   // if fopen fails, return false. Do NOT fclose
+  int temp;
+  int total;
+  int error;
+
+  total = 0;
+  temp = 0;
+  error = 0;
+
+  while((error = fscanf(fid, "%d\n", &temp)) != EOF)
+  {
+    total += temp;
+  }
+
+  *sum = total;
   // if fopen succeeds, read integers using fscan (do not use fgetc)
   //
   // * sum stores the result of adding all numbers from the file
   // When no more numbers can be read, fclose, return true
   //
+  fclose(fid);
   return true;
 }
 
-
-
 bool writeSum(char * filename, int sum)
 {
+  FILE * fid;
   // open a file whose name is filename for writing
+  fid = fopen(filename, "w");
+  if (fid == NULL)
+  {
+    printf("Come on you guys, there it is right there in front of you the whole time. You're dereferencing a null pointer! Open your eyes!");
+    return false;
+  }
+  fprintf(fid, "%d\n", sum);
+
   // if fopen succeeds, write sum as an integer using fprintf
   // fprintf should use one newline '\n'
   // fclose, return true
   //
+  fclose(fid);
   return true;
 }
